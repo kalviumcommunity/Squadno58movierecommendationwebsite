a9e0a27696143b7babb17f6d8050cd5351187c4c
ayushsingh257 <ayush.singh@kalvium.community>
2024-02-26 11:29:04 +0530

    moviebranch

--- config/db.js
+++ config/db.js
@@ -2,4 +2,8 @@ const mongoose = require('mongoose');
 require("dotenv").config()
 const connection=mongoose.connect(process.env.MONGO_URL)
 
-module.exports={connection}
+const isConnected = () => {
+    return mongoose.connection.readyState === 1;
+  }
+
+module.exports={connection,isConnected}
